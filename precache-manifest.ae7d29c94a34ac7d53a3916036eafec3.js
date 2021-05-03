850cd73a8352778fcb9d4829ba25e65319768456
Cha_Y_S <vaite714@gmail.com>
2021-05-03 14:53:38 +0900

    Updates

---
+++ precache-manifest.ae7d29c94a34ac7d53a3916036eafec3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558046eb76e50eca3efd904d52460a61",
+    "url": "/HAI/index.html"
+  },
+  {
+    "revision": "3dd4c97ac701f28ab1fa",
+    "url": "/HAI/static/css/main.9f28c626.chunk.css"
+  },
+  {
+    "revision": "7e1501d8e7227060729c",
+    "url": "/HAI/static/js/2.96940ec7.chunk.js"
+  },
+  {
+    "revision": "0b4c429200d74988aa2f1657a298a43f",
+    "url": "/HAI/static/js/2.96940ec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dd4c97ac701f28ab1fa",
+    "url": "/HAI/static/js/main.2427dc11.chunk.js"
+  },
+  {
+    "revision": "8e58be0f16ebcd5424db",
+    "url": "/HAI/static/js/runtime-main.94cf5f1f.js"
+  },
+  {
+    "revision": "19fb8872512560bb846464e91ae44bbd",
+    "url": "/HAI/static/media/1.19fb8872.jpg"
+  }
+]);
